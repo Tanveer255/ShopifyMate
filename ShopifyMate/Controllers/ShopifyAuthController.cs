8a2107f99b2b8d76d5f8e4acbe1ac158b41ac632
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-04-07 13:46:56 +0500

    Global using added

--- ShopifyMate/Controllers/ShopifyAuthController.cs
+++ ShopifyMate/Controllers/ShopifyAuthController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ShopifyMate.Controllers;
 [Route("api/[controller]")]
--- ShopifyMate/Controllers/ShopifyWebhookController.cs
+++ ShopifyMate/Controllers/ShopifyWebhookController.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.Security.Cryptography;
-using System.Text.Json;
-using System.Text;
+﻿using System.Security.Cryptography;
 
 namespace ShopifyMate.Controllers;
 
