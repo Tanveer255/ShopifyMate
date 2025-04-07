5d6db8dcea7c3f91919243e83d5fa99005f34398
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-04-07 13:55:49 +0500

    AppSetting and shopify setting added

---
+++ ShopifyMate/Entity/Common/Models/AppSetting.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopifyMate.Entity.Common.Models;
+
+public class AppSetting
+{
+    public Shopify Shopify { get; set; }
+}
+public class Shopify
+{
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string Scopes { get; set; }
+    public string RedirectUri { get; set; }
+}
